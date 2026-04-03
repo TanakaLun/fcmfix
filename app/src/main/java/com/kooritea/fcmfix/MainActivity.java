908e0aee58f7bb08f5b432bd7cd42d70537d3c98
TanakaLun <2709410138@qq.com>
2026-04-04 01:15:00 +0800

    fix:  e2e

--- app/src/main/java/com/kooritea/fcmfix/MainActivity.java
+++ app/src/main/java/com/kooritea/fcmfix/MainActivity.java
@@ -23,9 +23,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.checkbox.MaterialCheckBox;
 import com.google.android.material.color.DynamicColors;
