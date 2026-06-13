ee3cfb63d0d7fa9cf964fba69ce23c626b391386
TanakaLun <2709410138@qq.com>
2026-06-14 02:07:45 +0800

    fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
     compileOnly(libs.libxposed.api)
-    implementation(libs.libxposed.interface)
+    implementation(libs.libxposed.`interface`)
     implementation(libs.libxposed.service)
     implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation(libs.androidx.activity)
